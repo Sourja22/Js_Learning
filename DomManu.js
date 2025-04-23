9786d24cc57dfb6c60d4d74c313d6c541538b525
Sourja <sourja.dhar@cloudkaptan.com>
2025-04-23 08:59:07 +0530

    DOM_Manupulation

--- DomManu.js
+++ DomManu.js
@@ -12,5 +12,19 @@
 //querySelectorAll- Always returns  an array
 //querySelector- Returns single element
 
-var ele= document.querySelectorAll('.para');
-console.log(ele[1].textContent);
+// var ele= document.querySelectorAll('.para');
+// console.log(ele[1].textContent);
+
+
+//appending child to my div from JS
+var ele= document.getElementById('para-id');
+var para= document.createElement('p');
+para.innerHTML= 'I created you on my Work anniversary';
+ele.appendChild(para);
+
+//removing the child by refering to parent
+// ele.removeChild(para);
+
+
+// removing the child directly.
+// para.remove();
